f5035631d4195f69c939ca251c11c2039d58a8e9
jacksoncougar <jacksoncougar@gmail.com>
2018-05-04 12:45:20 -0600

    consistent

--- PapyrusVR_Module/api/collisions/Sphere.cpp
+++ PapyrusVR_Module/api/collisions/Sphere.cpp
@@ -5,6 +5,6 @@ namespace PapyrusVR
 	bool Sphere::CheckForOverlap(Vector3 distanceFromOrigin)
 	{
 		//Use square distances (more performance friendly)
-		return pow(distanceFromOrigin.x, 2) + pow(distanceFromOrigin.y, 2) + pow(distanceFromOrigin.z, 2) < (radius*radius);
+        return pow( distanceFromOrigin.x, 2 ) + pow( distanceFromOrigin.y, 2 ) + pow( distanceFromOrigin.z, 2 ) < pow( radius, 2 );
 	}
 }
